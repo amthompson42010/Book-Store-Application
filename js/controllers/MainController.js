39deeb09f0caf6007a9fb7b0baee4523a8a5c91f
Alexander Mark Thompson <amthompson42010@gmail.com>
2016-05-30 19:20:03 -0500

    Made a library store applicaiton using AngularJS

---
+++ js/controllers/MainController.js
@@ -0,0 +1,42 @@
+app.controller('MainController', ['$scope',  	function($scope) 
+	{ $scope.title = 'Alexs App';
+    $scope.promo = "Hello";
+    $scope.products = [
+      {
+        name: 'The Book of Trees',
+        price: 19,
+        pubdate: new Date('2014', '03', '08'),
+        cover: 'img/the-book-of-trees.jpg',
+        likes: 0,
+        dislikes: 0
+      },
+      {
+        name: 'Program or be Programmed',
+        price: 8,
+        pubdate: new Date('2013', '08', '01'),
+        cover: 'img/program-or-be-programmed.jpg',
+        likes: 0,
+        dislikes: 0
+      },
+      {
+      	name: 'Prison School',
+        price: 20,
+        pubdate: new Date('2014', '01', '22'),
+        likes: 0,
+        dislikes: 0
+      },
+      {
+      	name: 'X-Men',
+        price: 10,
+        pubdate: new Date('2014', '12', '02'),
+        likes: 0,
+        dislikes: 0
+      }
+    ];
+    $scope.plusOne = function(index) {
+      $scope.products[index].likes += 1;
+    };
+    $scope.minusOne = function(index) {
+      $scope.products[index].dislikes += 1;
+    };
+	}]);
